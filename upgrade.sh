50856784c66ea08895c0fc8ab4bb12801b0c801a
Arrow Zhang <arrow.zhang@shopee.com>
2021-10-20 15:16:34 +0800

    config

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker-compose -p atx2 -f docker-compose.yml down
-docker-compose -p atx2 -f docker-compose.ym build
-docker-compose -p atx2 -f docker-compose.ym up -d
+docker-compose -p atx2 -f docker-compose.yml build
+docker-compose -p atx2 -f docker-compose.yml up -d
 #docker-compose -p atx2 -f docker-compose.ym restart
